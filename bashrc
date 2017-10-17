3582ba3da40f9a263f0a6ee91f17d591ad7731c2
Michael <michaellim@surveymonkey.com>
2017-10-17 08:07:24 -0700

    Initial commit

---
+++ bashrc
@@ -0,0 +1,3 @@
+alias py="/usr/local/bin/python3"
+
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
---
+++ make_links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles            # dotfiles directory
+olddir=~/dotfiles_old     # old dotfiles backup directory
+files="bashrc zshrc"      # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating dotfiles_old in homedir"
+mkdir -p $olddir
+echo "...done"
+
+# change to dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do 
+    echo "Moving $file from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in ~"
+    ln -s $dir/$file ~/.$file
+done
